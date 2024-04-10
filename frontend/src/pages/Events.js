1c8dd708edb2bb698a3e9c8bff176fc70d91f9b2
MarcoBasileDev <marco.basile1996@gmail.com>
2024-04-10 10:16:45 +0200

    refactoring error using json function

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
@@ -13,9 +13,15 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    throw new Response(JSON.stringify({ message: "Could not fecth events." }), {
+    /* throw new Response(JSON.stringify({ message: "Could not fecth events." }), {
       status: 500,
-    });
+    }); */
+    return json(
+      { message: "Could not fecth events." },
+      {
+        status: 500,
+      }
+    );
   } else {
     return response;
   }
