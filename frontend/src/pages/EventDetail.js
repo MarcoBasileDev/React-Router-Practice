e80b6bae841ea96673d8f53907c16a18b298380c
MarcoBasileDev <marco.basile1996@gmail.com>
2024-04-10 11:00:39 +0200

    add eventDetail route

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,12 +1,24 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 export default function EventDetail() {
-  const params = useParams();
+  const data = useLoaderData();
 
-  return (
-    <>
-      <h1>event detail</h1>
-      <p>Event ID: {params.eventId}</p>
-    </>
-  );
+  return <EventItem event={data.event} />;
+}
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fecth details for selected event." },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    return response;
+  }
 }
